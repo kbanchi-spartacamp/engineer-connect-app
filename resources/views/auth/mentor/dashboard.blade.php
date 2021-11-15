f4a7caf42e7c5d0333308853279b610093bd51b7
kbanchi1111 <kbanchi.1111@gmail.com>
2021-11-15 11:44:34 +0900

    update dashboard: add link page

--- resources/views/auth/mentor/dashboard.blade.php
+++ resources/views/auth/mentor/dashboard.blade.php
@@ -1,14 +1,20 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <ul>
+                    <li><a class="hover:text-blue-500 text-green-500 font-bold" href="/mentor/login">メンターログイン</a></li>
+                    <li><a class="hover:text-blue-500 text-green-500 font-bold" href="/mentor/register">サインアップサインアップ</a>
+                    </li>
+                    <li><a class="hover:text-blue-500 text-green-500 font-bold" href="/mentor/profile">メンタープロフィール</a>
+                    </li>
+                    <li><a class="hover:text-blue-500 text-green-500 font-bold"
+                            href="/mentor_schedules/1/reservations">予約一覧</a></li>
+                    <li><a class="hover:text-blue-500 text-green-500 font-bold"
+                            href="/mentors/1/mentor_skills/create">スキル設定</a></li>
+                    <li><a class="hover:text-blue-500 text-green-500 font-bold"
+                            href="/mentor_schedules/create">スケジュール設定</a></li>
+                </ul>
             </div>
         </div>
     </div>
