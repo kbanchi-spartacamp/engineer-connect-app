ffe29c86f70760917c870b9d6107483bda0f5345
kbanchi1111 <kbanchi.1111@gmail.com>
2021-11-16 12:52:44 +0900

    update schedule search (add date and category)

--- app/Http/Controllers/MentorScheduleController.php
+++ app/Http/Controllers/MentorScheduleController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\MentorSchedule;
 use App\Models\SkillCategory;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 
 class MentorScheduleController extends Controller
 {
@@ -15,7 +16,25 @@ class MentorScheduleController extends Controller
      */
     public function index()
     {
-        return view('mentor_schedules.index');
+        $dates = [];
+        $date = now();
+        for ($i = 0; $i < 7; $i++) {
+            $dates[] = $date->formatLocalized('%m/%d(%a)');
+            $date = $date->addDay();
+        }
+
+        $skillCategories = SkillCategory::all();
+
+        $date = now();
+        $start_time = $date->addMinutes(30 - $date->minute % 30);
+        $end_time = new Carbon('24:00:00');
+        $times = [];
+        while ($start_time < $end_time) {
+            $times[] = $start_time->format('H:i');
+            $start_time = $start_time->addMinute(30);
+        }
+
+        return view('mentor_schedules.index', compact('dates', 'skillCategories', 'times'));
     }
 
     /**
@@ -25,7 +44,7 @@ class MentorScheduleController extends Controller
      */
     public function create()
     {
-        
+
         return view('mentor_schedules.create');
     }
 
--- resources/views/mentor_schedules/index.blade.php
+++ resources/views/mentor_schedules/index.blade.php
@@ -2,7 +2,86 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                mentor_schedules.index
+                <ul class="flex">
+                    <li class="ml-10">
+                        @foreach ($dates as $date)
+                            <a href="" class="text-3xl hover:text-blue-500">{{ $date }}</a>
+                        @endforeach
+                    </li>
+                </ul>
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="flex bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                        <div class="m-4">
+                            <select name="skillCategory_id">
+                                @foreach ($skillCategories as $skillCategory)
+                                    <option value="{{ $skillCategory->id }}">{{ $skillCategory->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="m-4">
+                            <select name="" id="">
+                                @foreach ($times as $time)
+                                    <option value="{{ $time }}" @if ($loop->index == 0) selected @endif>{{ $time }}
+                                    </option>
+                                @endforeach
+                            </select>
+                            <span>〜</span>
+                            <select name="" id="">
+                                @foreach ($times as $time)
+                                    <option value="{{ $time }}" @if ($loop->index == count($times) - 1) selected @endif>{{ $time }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+                        <div class="m-4">
+                            <input type="submit" value="ブックマーク"
+                                class="w-full sm:w-40 bg-gradient-to-r from-indigo-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 p-2 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500 w-full sm:w-32">
+                        </div>
+                        <div class="m-4">
+                            <input type="submit" value="検索"
+                                class="w-full sm:w-40 bg-gradient-to-r from-indigo-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 p-2 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500 w-full sm:w-32">
+                        </div>
+                    </div>
+                </div>
+                {{-- メンター一覧 --}}
+                <div>
+                    <div class="w-full">
+                        <div class="bg-white w-full px-10 py-8 hover:shadow-2xl transition duration-500">
+                            <div class="mt-4">
+                                <div class="flex justify-between text-sm items-center mb-4">
+                                    <div class="text-gray-700">
+                                        <div>年齢:20代</div>
+                                        <div>講師歴:3年</div>
+                                    </div>
+                                    <div class="border border-gray-900 px-2 h-7 leading-7 rounded-full text-right">
+                                        ★★★☆☆</div>
+                                </div>
+                                <h2 class="text-lg text-gray-700 font-semibold">Kawata</h2>
+                                <div class="flex justify-between items-center">
+                                    <div class="mt-4 flex items-center space-x-4 py-6">
+                                        <div>
+                                            <img class="rounded-full object-cover" src="" alt="" />
+                                        </div>
+                                    </div>
+                                    <div class="flex">
+                                        <a href=""
+                                            class="flex justify-center bg-gradient-to-r from-indigo-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 mt-4 px-5 py-3 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500 mx-2">18:00</a>
+                                        <a href=""
+                                            class="flex justify-center bg-gradient-to-r from-indigo-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 mt-4 px-5 py-3 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500 mx-2">18:30</a>
+                                        <a href=""
+                                            class="flex justify-center bg-gradient-to-r from-indigo-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 mt-4 px-5 py-3 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500 mx-2">19:00</a>
+                                        <a href=""
+                                            class="flex justify-center bg-gradient-to-r from-indigo-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 mt-4 px-5 py-3 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500 mx-2">19:30</a>
+                                        <a href=""
+                                            class="flex justify-center bg-gradient-to-r from-indigo-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 mt-4 px-5 py-3 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500 mx-2">20:00</a>
+                                        <a href=""
+                                            class="flex justify-center bg-gradient-to-r from-indigo-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 mt-4 px-5 py-3 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500 mx-2">20:30</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
