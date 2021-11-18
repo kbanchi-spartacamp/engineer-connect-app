d527161e03334ee051bb8f5b0473c464272e7b14
kbanchi1111 <kbanchi.1111@gmail.com>
2021-11-18 12:25:59 +0900

    update welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -412,17 +412,17 @@
     <div class="relative min-h-screen flex ">
         <div
             class="flex flex-col sm:flex-row items-center md:items-start sm:justify-center md:justify-start flex-auto min-w-0 bg-white">
-            <div class="h-full flex flex-auto bg-purple-900 text-white bg-no-repeat bg-cover relative bg-image">
+            <div class="h-full flex flex-auto bg-purple-900 text-gray bg-no-repeat bg-cover relative bg-image">
                 <div
                     class="w-4/5 flex flex-auto flex-col md:flex-row items-center justify-center p-10 xl:p-32 overflow-hidden">
-                    <div class="absolute bg-gradient-to-b from-indigo-900 to-black opacity-75 inset-0 z-0"></div>
+                    <div class="absolute from-red-900 to-gray opacity-75 inset-0 z-0"></div>
                     <div class="w-4/5 z-10">
-                        <h2 class="text-xl xl:text-3xl italic mb-6">○○○</h2>
+                        <h2 class="text-5xl xl:text-6xl italic mb-6">now conn</h2>
                         <div class="text-5xl sm:text-6xl xl:text-6xl font-bold leading-tight mb-6">
-                            困ったとき<br>聞きたいときに<br>現役エンジニアに。
+                            困った「今」<br>現役エンジニアに。
                         </div>
-                        <div class="sm:text-sm xl:text-md text-gray-200 font-normal">
-                            ○○○はエンジニアの「いま、困っている」と、現役エンジニアをマッチングするサービスです。</div>
+                        <div class="sm:text-sm xl:text-md text-black-200 font-normal">
+                            now conn は、「いま、困っている」エンジニアの悩みを解決するサービスです。</div>
                     </div>
                     <div
                         class="md:flex md:items-center md:justify-center w-full sm:w-auto md:h-full w-2/5 xl:w-2/5 p-8 md:p-10 lg:p-14 sm:rounded-lg md:rounded-none relative z-11">
