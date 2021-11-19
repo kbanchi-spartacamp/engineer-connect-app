154574e00cb4005e854b21d98ded84a0b69444ae
kbanchi1111 <kbanchi.1111@gmail.com>
2021-11-19 13:42:53 +0900

    change welcome button

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,7 +23,7 @@
                                 </h3>
                                 <div>
                                     <a href="{{ route('user.login') }}"
-                                        class="w-full flex justify-center bg-gradient-to-r from-indigo-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 p-4 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500">Sign
+                                        class="w-full flex justify-center bg-gradient-to-r from-yellow-400 to-yellow-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 p-4 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500">Sign
                                         in</a>
                                 </div>
                                 <p class="flex flex-col items-center justify-center mt-10 text-center text-md">
@@ -42,7 +42,7 @@
                                 </h3>
                                 <div>
                                     <a href="{{ route('mentor.login') }}"
-                                        class="w-full flex justify-center bg-gradient-to-r from-pink-500 to-purple-600 hover:bg-gradient-to-l hover:from-purple-500 hover:to-pink-600 text-gray-100 p-4 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500">Sign
+                                        class="w-full flex justify-center bg-gradient-to-r from-green-400 to-green-600 hover:bg-gradient-to-l hover:from-purple-500 hover:to-pink-600 text-gray-100 p-4 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500">Sign
                                         in</a>
                                 </div>
                                 <p class="flex flex-col items-center justify-center mt-10 text-center text-md">
