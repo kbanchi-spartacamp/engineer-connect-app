5e151c07b5cc17050bfcd0f3a3840cb9ff2a4945
akinobu <siguiente.football.373@gmail.com>
2021-11-19 15:04:29 +0900

    登録したメンターのカテゴリーID非表示がわからないです。

--- resources/views/mentors/mentor_skills/create.blade.php
+++ resources/views/mentors/mentor_skills/create.blade.php
@@ -4,31 +4,31 @@
             <div class="flex justify-center mb-6">
                 <h2>スキル設定画面</h2>
             </div>
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg pt-3">
                 <div class="flex justify-center mt-1 mb-3">
-                    <img src="{{ $user->profile_photo_url }}" class="h-10 w-10 rounded-full object-cover mr-3">
+                    <img src="{{ $user->profile_photo_url }}" class="rounded-full object-cover mr-3 w-1/7">
                 </div>
-                <div class="flex justify-center ">
-                    <table class="table-auto">
+                <div class="flex justify-center">
+                    <table class="table-fixed w-5/6 mb-7">
                         <thead>
                             <tr>
-                                <th class="px-4 py-2">SkillName</th>
-                                <th class="px-4 py-2">Year</th>
-                                <th class="px-4 py-2"></th>
+                                <th class="px-4 py-2 w-1/6 text-left">SkillName</th>
+                                <th class="px-4 py-2 text-left">Year</th>
+                                <th class="px-4 py-2 w-1/6"></th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($user->mentor_skills as $mentor_skill)
-                            <tr>
-                                <td class="border px-4 py-2">{{ $mentor_skill->skill_category->name }}</td>
-                                <td class="border px-4 py-2">{{ $mentor_skill->experience_year }}</td>
-                                <td class="border px-4 py-2">
+                            <tr class="border">
+                                <td class="px-4 py-2 ">{{ $mentor_skill->skill_category->name }}</td>
+                                <td class="px-4 py-2">{{ $mentor_skill->experience_year }}年</td>
+                                <td class="px-4 py-2 text-center w-full">
                                     <form action="{{ route('mentors.mentor_skills.destroy', [$user, $mentor_skill]) }}"
-                                        method="post" class="w-full sm:w-32">
+                                        method="post" class="w-full  ">
                                         @csrf
                                         @method('DELETE')
-                                        <input type="submit" value="削除"
-                                            onclick="if(!confirm('削除しますか？')){return false};">
+                                        <input type="submit" value="削除" onclick="if(!confirm('削除しますか？')){return false};"
+                                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
                                     </form>
                                 </td>
                             </tr>
@@ -41,11 +41,11 @@
                     <form action="{{ route('mentors.mentor_skills.store', $user) }}" method="POST"
                         class="rounded pt-3 pb-8 ">
                         @csrf
-                        <div class="mb-4">
-                            <label>
+                        <div class="mb-10">
+                            <label class="mr-6 ">
                                 対応スキル
                             </label>
-                            <select name="skill_category_id">
+                            <select name="skill_category_id" class="rounded ">
                                 <option disabled selected value="">選択してください</option>
                                 @foreach ($skill_categories as $skill_category)
                                 <option value="{{ $skill_category->id }}" @if ($skill_category->id ==
@@ -56,17 +56,21 @@
                             </select>
                         </div>
                         <div>
-                            <label>
+                            <label class="mr-5">
                                 対応年数
                             </label>
                             <input type="text" name="experience_year" required placeholder="対応年数"
-                                value="{{ old('experience_year') }}">
+                                value="{{ old('experience_year') }}" class="rounded">
                         </div>
                 </div>
 
-                <div class="flex justify-center">
-                    <a href="" class="mr-4">戻る</a>
-                    <input type="submit" value="更新">
+                <div class="flex justify-center mb-5">
+                    <a href=""
+                        class="mr-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                        戻る
+                    </a>
+                    <input type="submit" value="更新"
+                        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                     </form>
                 </div>
 
