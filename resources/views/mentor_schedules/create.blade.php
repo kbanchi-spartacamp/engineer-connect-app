15f776061e7785bb2d79cf7a2c0cf0a0952ebb4c
akinobu <siguiente.football.373@gmail.com>
2021-11-20 13:21:53 +0900

    mentorの見た目ほぼ完成

--- resources/views/mentor_schedules/create.blade.php
+++ resources/views/mentor_schedules/create.blade.php
@@ -1,94 +1,132 @@
 <x-app-layout>
-    <div class="py-8">
+    <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex justify-center mb-6 mr-5">
+            <div class="flex justify-center mb-4">
                 <h2>スケジュール設定画面</h2>
             </div>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="flex justify-center mt-1 mb-3 mr-2">
-                    <img src="{{ $user->profile_photo_url }}" class="h-10 w-10 rounded-full object-cover mr-3">
+                <div class="flex justify-center mt-4 mb-5 ">
+                    <img src="{{ $user->profile_photo_url }}" class="rounded-full object-cover w-1/7 ">
                 </div>
-                <form action="{{ route('mentor_schedules.store') }}" method="POST" class="rounded pt-3 pb-4 ">
+                <form action="{{ route('mentor_schedules.store') }}" method="POST" class="rounded pb-4 ">
                     @csrf
-                    <p class="flex justify-center mb-5 mr-6">
+                    <p class="flex justify-center mb-5 ">
                         本日の予定を登録
                     </p>
-                    <div class="flex justify-center ">
-                        <button class="mr-8 pl-3 text-left w-32  px-2 py- bg-white border border-black rounded">本日</button>
-                        <select name="today_start_time" class="mr-6 rounded">
-                            @foreach ($times as $time)
-                            <option value="{{ $time }}" @if ($loop->index == 0) selected @endif>
-                                {{ $time }}
-                            </option>
-                            @endforeach
-                        </select>
-                        <span class="pt-2 mr-6 ">〜</span>
-                        <select name="today_end_time" class="mr-6 rounded">
-                            @foreach ($times as $time)
-                            <option value="{{ $time }}" @if ($loop->index == 0) selected @endif  >
-                                {{ $time }}
-                            </option>
-                            @endforeach
-                        </select>
-                        <input type="submit" value="追加"
-                            class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">
-                    </div>
-                </form>
-                <form action="{{ route('mentor_schedules.store') }}" method="POST" class="rounded pt-3 pb-7 ">
-                    @csrf
-                    <p class="flex justify-center mb-7 mr-6" >定期的な予定を登録</p>
                     <div class="flex justify-center">
-                        <select name="day_of_week" class="mr-8 w-32 ml-9" class="text-csaienter">
-                            @foreach (DayOfWeekConst::DAY_OF_WEEK_LIST as $day_of_week)
-                            <option value="{{ $day_of_week }}" @if ($loop->index == 0) selected @endif >
-                                {{ array_search($day_of_week, DayOfWeekConst::DAY_OF_WEEK_LIST) }}
-                            </option>
-                            @endforeach
-                        </select>
-                        <select name="open_time" class="mr-6">
-                            @foreach ($open_times as $time)
-                            <option value="{{ $time }}" @if ($loop->index == 0) selected @endif>
-                                {{ $time }}
-                            </option>
-                            @endforeach
-                        </select>
-                        <label class="pt-2 mr-6">〜</label>
-                        <select name="end_time" class="mr-6">
-                            @foreach ($open_times as $time)
-                            <option value="{{ $time }}" @if ($loop->index == 0) selected @endif>
-                                {{ $time }}
-                            </option>
-                            @endforeach
-                        </select>
-                        <input type="submit" value="追加" class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded mr-9">
+                        <table class="table-fixed w-5/6 mb-5">
+                            <tbody>
+                                <tr>
+                                    <td class="px-4 w-1/4">
+                                        <select class="text-center rounded w-full  ">
+                                            <option>
+                                                本日
+                                            </option>
+                                        </select>
+                                    </td>
+                                    <td class="px-4 w-1/4">
+                                        <select name="today_start_time" class="text-center rounded w-full ">
+                                            @foreach ($times as $time)
+                                            <option value="{{ $time }}" @if ($loop->index == 0) selected @endif>
+                                                {{ $time }}
+                                            </option>
+                                            @endforeach
+                                        </select>
+                                    </td>
+                                    <td class="w-2 pr-4">
+                                        <span class="pt-2">〜</span>
+                                    </td>
+                                    <td class="px-4 w-1/4">
+                                        <select name="today_end_time" class="text-center rounded w-full">
+                                            @foreach ($times as $time)
+                                            <option value="{{ $time }}" @if ($loop->index == 0) selected @endif >
+                                                {{ $time }}
+                                            </option>
+                                            @endforeach
+                                        </select>
+                                    </td>
+                                    <td class="px-4 w-1/4">
+                                        <input type="submit" value="追加"
+                                            class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded w-full">
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                 </form>
-                <div class="flex justify-center mr-6">
-                    <h2>不定期なスケジュール</h2>
-                </div>
-                <div>
-                    <table class="table-auto mb-7 flex justify-center">
+                <form action="{{ route('mentor_schedules.store') }}" method="POST" class="rounded">
+                    @csrf
+                    <p class="flex justify-center mb-5 ">定期的な予定を登録</p>
+                    <div class="flex justify-center">
+                        <table class="table-fixed w-5/6 mb-8">
+                            <tbody>
+                                <tr>
+                                    <td class="px-4 w-1/4">
+                                        <select name="day_of_week" class="text-center rounded w-full">
+                                            @foreach (DayOfWeekConst::DAY_OF_WEEK_LIST as $day_of_week)
+                                            <option value="{{ $day_of_week }}" @if ($loop->index == 0) selected @endif >
+                                                {{ array_search($day_of_week, DayOfWeekConst::DAY_OF_WEEK_LIST) }}
+                                            </option>
+                                            @endforeach
+                                        </select>
+                                    </td>
+                                    <td class="px-4 w-1/4">
+                                        <select name="open_time" class="text-center rounded w-full">
+                                            @foreach ($open_times as $time)
+                                            <option value="{{ $time }}" @if ($loop->index == 0) selected @endif>
+                                                {{ $time }}
+                                            </option>
+                                            @endforeach
+                                        </select>
+                                    </td>
+                                    <td class="w-2 pr-4">
+                                        <label class="pt-2 ">〜</label>
+                                    </td>
+                                    <td class="px-4 w-1/4">
+                                        <select name="end_time" class="text-center rounded w-full">
+                                            @foreach ($open_times as $time)
+                                            <option value="{{ $time }}" @if ($loop->index == 0) selected @endif>
+                                                {{ $time }}
+                                            </option>
+                                            @endforeach
+                                        </select>
+                                    </td>
+                                    <td class="px-4 w-1/4">
+                                        <input type="submit" value="追加"
+                                            class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded w-full">
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </form>
+
+                <p class="flex justify-center mb-2 ">不定期なスケジュール</p>
+
+                <div class="flex justify-center">
+                    <table class="table-fixed w-5/6 mb-7">
                         <thead>
                             <tr>
-                                <th class="px-4 py-2">Timing</th>
-                                <th class="px-4 py-2">Start</th>
-                                <th class="px-4 py-2">-</th>
+                                <th class="px-4 py-2 w-1/4 text-left">Timing</th>
+                                <th class="px-4 py-2 text-left">Start</th>
+                                <th class="py-2 w-1/4"></th>
                             </tr>
                         </thead>
                         <tbody>
                             @foreach ($mentorIrregularSchedules as $mentorIrregularSchedules)
-                            <tr>
-                                <td class="border px-4 py-2">
-                                    {{ $mentorIrregularSchedules->day }}
+                            <tr class="border">
+                                <td class="px-4 py-2">
+                                    {{ $mentorIrregularSchedules->day->format('n/j') }}
                                 </td>
-                                <td class="border px-4 py-2">
+                                <td class="px-4 py-2">
                                     {{ $mentorIrregularSchedules->start_time }}</td>
-                                <td class="border px-4 py-2">
+                                <td class="px-4 py-2 w-full">
                                     <form action="{{ route('mentor_schedules.destroy', $mentorIrregularSchedules) }}"
-                                        method="post" class="w-full sm:w-32">
+                                        method="post" class="w-full ">
                                         @csrf
                                         @method('DELETE')
-                                        <input type="submit" value="削除" onclick="if(!confirm('削除しますか？')){return false};">
+                                        <input type="submit" value="削除" onclick="if(!confirm('削除しますか？')){return false};"
+                                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-full">
                                     </form>
                                 </td>
                             </tr>
@@ -96,36 +134,41 @@
                         </tbody>
                     </table>
                 </div>
-                <h2>定期的なスケジュール</h2>
-                <table class="table-auto">
-                    <thead>
-                        <tr>
-                            <th class="px-4 py-2">Timing</th>
-                            <th class="px-4 py-2">Start</th>
-                            <th class="px-4 py-2">-</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach ($mentorRegularSchedules as $mentorRegularSchedule)
-                        <tr>
-                            <td class="border px-4 py-2">
-                                {{ array_search($mentorRegularSchedule->day_of_week, DayOfWeekConst::DAY_OF_WEEK_LIST)
-                                }}
-                            </td>
-                            <td class="border px-4 py-2">
-                                {{ $mentorRegularSchedule->start_time }}</td>
-                            <td class="border px-4 py-2">
-                                <form action="{{ route('mentor_schedules.destroy', $mentorRegularSchedule) }}"
-                                    method="post" class="w-full sm:w-32">
-                                    @csrf
-                                    @method('DELETE')
-                                    <input type="submit" value="削除" onclick="if(!confirm('削除しますか？')){return false};">
-                                </form>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
+
+                <p class="flex justify-center mb-2 ">定期的なスケジュール</p>
+                <div class="flex justify-center">
+                    <table class="table-fixed w-5/6 mb-7">
+                        <thead>
+                            <tr>
+                                <th class="px-4 py-2 w-1/4 text-left">Timing</th>
+                                <th class="px-4 py-2 text-left">Start</th>
+                                <th class="py-2 w-1/4"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($mentorRegularSchedules as $mentorRegularSchedule)
+                            <tr>
+                                <td class="border px-4 py-2">
+                                    {{ array_search($mentorRegularSchedule->day_of_week,
+                                    DayOfWeekConst::DAY_OF_WEEK_LIST)
+                                    }}
+                                </td>
+                                <td class="border px-4 py-2">
+                                    {{ $mentorRegularSchedule->start_time }}</td>
+                                <td class="border px-4 py-2 w-full">
+                                    <form action="{{ route('mentor_schedules.destroy', $mentorRegularSchedule) }}"
+                                        method="post" class="w-full ">
+                                        @csrf
+                                        @method('DELETE')
+                                        <input type="submit" value="削除" onclick="if(!confirm('削除しますか？')){return false};"
+                                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded w-full">
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
--- resources/views/mentors/mentor_skills/create.blade.php
+++ resources/views/mentors/mentor_skills/create.blade.php
@@ -6,7 +6,7 @@
             </div>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg pt-3">
                 <div class="flex justify-center mt-1 mb-3">
-                    <img src="{{ $user->profile_photo_url }}" class="rounded-full object-cover mr-3 w-1/7">
+                    <img src="{{ $user->profile_photo_url }}" class="rounded-full object-cover w-1/7">
                 </div>
                 <div class="flex justify-center">
                     <table class="table-fixed w-5/6 mb-7">
@@ -41,10 +41,10 @@
                         class="rounded pt-3 pb-8 ">
                         @csrf
                         <div class="mb-10">
-                            <label class="mr-6 ">
+                            <label class="mr-3 w-3/10">
                                 対応スキル
                             </label>
-                            <select name="skill_category_id" class="rounded ">
+                            <select name="skill_category_id" class="rounded ml-4 w-6/10">
                                 <option disabled selected value="">選択してください</option>
                                 @foreach ($skill_categories as $skill_category)
                                 <option value="{{ $skill_category->id }}" @if ($skill_category->id ==
@@ -55,20 +55,20 @@
                             </select>
                         </div>
                         <div>
-                            <label class="mr-5">
+                            <label class="mr-3 w-3/10">
                                 対応年数
                             </label>
                             <input type="text" name="experience_year" required placeholder="対応年数"
-                                value="{{ old('experience_year') }}" class="rounded">
+                                value="{{ old('experience_year') }}" class="rounded w-6/10 ml-3">
                         </div>
                 </div>
                 <div class="flex justify-center mb-5">
                     <a href=""
-                        class="mr-4 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+                        class="mr-2 bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
                         戻る
                     </a>
                     <input type="submit" value="更新"
-                        class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">
+                        class="bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded ml-2">
                     </form>
                 </div>
             </div>
