45e6e8be9eab31203f904e8f32b76bc51d241327
ToshikiKawata <kawatatohsiki@icloud.com>
2021-11-20 12:10:53 +0900

    予約画面見た目整え

--- resources/views/reservations/show.blade.php
+++ resources/views/reservations/show.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="py-12">
-        <div>以下のスケジュールで予約しました</div>
+        <div class="flex justify-center text-3xl">以下のスケジュールで予約しました</div>
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 mentors.show
@@ -14,17 +14,17 @@
                             <h2 class="font-bold font-sans break-normal text-gray-900 pt-6 pb-1 text-3xl md:text-4xl">
                                 {{ $reservation->mentor->name }}</h2>
                             <div
-                                class="flex justify-center bg-gradient-to-r mt-4 px-5 py-3 rounded-full tracking-wide font-semibold duration-500 mx-2">
+                                class="flex justify-center text-xl bg-gradient-to-r text-gray-900 mt-4 px-5 py-3 rounded tracking-wide font-semibold mx-2">
                                 {{ $reservation->start_time->format('Y-m-d') }}
                             </div>
                             <div
-                                class="flex justify-center bg-gradient-to-r mt-4 px-5 py-3 rounded-full tracking-wide font-semibold duration-500 mx-2">
+                                class="flex justify-center text-xl bg-gradient-to-r text-gray-900 mt-4 px-5 py-3 rounded tracking-wide font-semibold mx-2">
                                 {{ $reservation->start_time->format('H:i') }}
                             </div>
-                            <div class="m-4">
-                                <input type="submit" value="メッセージ"
-                                    class="w-full sm:w-40 bg-gradient-to-r from-indigo-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 p-2 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500 w-full sm:w-32">
-                            </div>
+                        </div>
+                        <div class="flex justify-center">
+                            <input type="submit" value="メッセージ"
+                                class="w-full sm:w-40 bg-gradient-to-r from-indigo-500 to-blue-600 hover:bg-gradient-to-l hover:from-blue-500 hover:to-indigo-600 text-gray-100 p-2 rounded-full tracking-wide font-semibold shadow-lg cursor-pointer transition ease-in duration-500 w-full sm:w-32">
                         </div>
                     </div>
                 </article>
