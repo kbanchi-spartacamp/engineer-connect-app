bbc5f5bbf1789960bad807441229e65212684448
kbanchi1111 <kbanchi.1111@gmail.com>
2021-11-21 13:21:36 +0900

    add kegak page

--- resources/views/legal.blade.php
+++ resources/views/legal.blade.php
@@ -1,3 +1,39 @@
 <x-guest-layout>
-    <h2>利用規約</h2>
+    <div id="whoobe-1yakq"
+        class="bg-white text-base w-5/6 md:w-1/2 lg:w-2/3 rounded-lg shadow m-auto -brown-400 border border-t-8 border-b-8 border-black mt-20 flex flex-col">
+        <div id="whoobe-pa28x" class="p-4 flex flex-col">
+            <h3 type="element" class="text-4xl" id="whoobe-ozjao">利用規約</h3>
+            <p type="element" class="my-4" id="whoobe-dvhrz">
+                この規約（以下「本規約」といいます）は、MENTA株式会社（以下「当社」といいます）が提供する、個人間や個人と法人間でメンタリングによる指導、支援又は育成をしたい方とされたい方がマッチングできるサービス「MENTA」（以下「本サービス」といいます）の利用に関する条件を、本サービスを利用する者（以下「ユーザー」といいます）と当社の間で定めるものです。本規約は本サービスの利用に関して生ずるすべての関係に適用されるものとし、ユーザーは、本規約を熟読し、本規約の内容を十分に理解した上でこれを承諾して、本サイトを利用するものとします。本サービスを利用することで、下記の利用規約に同意していただいたこととみなさせていただきますのでご注意ください。
+            </p>
+            <div class="m-3">
+                <h3 type="element" class="text-2xl">第1条（本サービスの利用）</h3>
+                <ol type="element" class="ml-4 list-decimal">
+                    <li>ユーザーは、本規約の定めに従って当サービスを利用しなければなりません。</li>
+                    <li>ユーザーは、申し込みフォームを送信すること又は本サービスを実際に利用することによって、本規約の内容及び本規約が本サービスの利用契約の内容となることに同意したものとみなされます。</li>
+                </ol>
+            </div>
+            <h3 type="element" class="text-2xl">第２条（利用者登録）</h3>
+            <h3 type="element" class="text-2xl">第３条（利用規約の変更について）</h3>
+            <h3 type="element" class="text-2xl">第４条（非保証・免責）</h3>
+            <h3 type="element" class="text-2xl">第５条（通知または連絡）</h3>
+            <h3 type="element" class="text-2xl">第６条（IDやパスワード等の管理）</h3>
+            <h3 type="element" class="text-2xl">第7条（個人情報の取り扱い第8条（本サービスの利用条件）第1条（本サービスの利用）</h3>
+            <h3 type="element" class="text-2xl">第8条（本サービスの利用条件）</h3>
+            <h3 type="element" class="text-2xl">第9条（本サービスの利用料及び支払方法に関する事項）</h3>
+            <h3 type="element" class="text-2xl">第10条（チップに関する事項）</h3>
+            <h3 type="element" class="text-2xl">第11条（退会）</h3>
+            <h3 type="element" class="text-2xl">第12条（メンタリング契約）</h3>
+            <h3 type="element" class="text-2xl">第13条（教材の販売）</h3>
+            <h3 type="element" class="text-2xl">第14条（報酬に関する注意事項）</h3>
+            <h3 type="element" class="text-2xl">第15条（ユーザー間の秘密保持）</h3>
+            <h3 type="element" class="text-2xl">第16条（知的財産権）</h3>
+            <h3 type="element" class="text-2xl">第17条（サービスの一時的な中断）</h3>
+            <h3 type="element" class="text-2xl">第18条（損害賠償）</h3>
+            <h3 type="element" class="text-2xl">第19条（免責事項）</h3>
+            <h3 type="element" class="text-2xl">第20条（本サービスの終了）</h3>
+            <h3 type="element" class="text-2xl">第21条（合意管轄等）</h3>
+            <h3 type="element" class="text-2xl">附則</h3>
+        </div>
+    </div>
 </x-guest-layout>
