4ed7e00b2984fdd71300f6b269f3460cd293b428
kbanchi1111 <kbanchi.1111@gmail.com>
2021-11-21 13:22:36 +0900

    add guide page

--- resources/views/guide.blade.php
+++ resources/views/guide.blade.php
@@ -1,3 +1,8 @@
 <x-guest-layout>
-    <h2>利用ガイド</h2>
+    <div id="whoobe-1yakq"
+        class="bg-white text-base w-5/6 md:w-1/2 lg:w-2/3 rounded-lg shadow m-auto -brown-400 border border-t-8 border-b-8 border-black mt-20 flex flex-col">
+        <div id="whoobe-pa28x" class="p-4 flex flex-col">
+            <h3 type="element" class="text-4xl" id="whoobe-ozjao">利用ガイド</h3>
+        </div>
+    </div>
 </x-guest-layout>
