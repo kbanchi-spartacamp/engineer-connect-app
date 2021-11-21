d56b3921cb189a2437a1d1c569cda37414e63957
akinobu <siguiente.football.373@gmail.com>
2021-11-21 12:54:01 +0900

    時間の過ぎた予約画面のグレーアウト

--- resources/views/reservations/index.blade.php
+++ resources/views/reservations/index.blade.php
@@ -6,26 +6,46 @@
             </div>
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 @foreach ($reservations as $reservation)
-                    <div class="container flex justify-center mx-auto my-2">
-                        @if (Auth::guard(UserConst::GUARD)->check())
-                            <img src="{{ $reservation->mentor->profile_photo_url }}"
-                                class="rounded-full w-1/7 mr-4 ml-10">
-                        @else
-                            <img src="{{ $reservation->user->profile_photo_url }}"
-                                class="rounded-full w-1/7 mr-4 ml-10">
-                        @endif
-                        <label class="flex justify-center items-center text-center w-1/4 text-3xl  ">
-                            {{ $reservation->day->format('n/j') }}
-                        </label>
-                        <label class="flex justify-center items-center text-center w-1/4 text-3xl mr-4">
-                            {{ $reservation->start_time->format('G:i') }} 〜
-                        </label>
-                        <a href="{{ route('users.mentors.messages.index', [$reservation->user, $reservation->mentor]) }}"
-                            class="flex justify-center items-center text-center bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">
-                            メッセージ
-                        </a>
-                    </div>
-                    <hr class="boild">
+                @if($reservation->start_time >= now())
+                <div class="container flex justify-center mx-auto my-2">
+                    @if (Auth::guard(UserConst::GUARD)->check())
+                    <img src="{{ $reservation->mentor->profile_photo_url }}" class="rounded-full w-1/7 mr-4 ml-10">
+                    @else
+                    <img src="{{ $reservation->user->profile_photo_url }}" class="rounded-full w-1/7 mr-4 ml-10">
+                    @endif
+                    <label class="flex justify-center items-center text-center w-1/4 text-3xl  ">
+                        {{ $reservation->day->format('n/j') }}
+                    </label>
+                    <label class="flex justify-center items-center text-center w-1/4 text-3xl mr-4">
+                        {{ $reservation->start_time->format('G:i') }} 〜
+                    </label>
+                    <a href="{{ route('users.mentors.messages.index', [$reservation->user, $reservation->mentor]) }}"
+                        class="flex justify-center items-center text-center bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">
+                        メッセージ
+                    </a>
+                </div>
+                <hr class="boild">
+
+                @else
+                <div class="container flex justify-center mx-auto my-2 bg-gray-500">
+                    @if (Auth::guard(UserConst::GUARD)->check())
+                    <img src="{{ $reservation->mentor->profile_photo_url }}" class="rounded-full w-1/7 mr-4 ml-10">
+                    @else
+                    <img src="{{ $reservation->user->profile_photo_url }}" class="rounded-full w-1/7 mr-4 ml-10">
+                    @endif
+                    <label class="flex justify-center items-center text-center w-1/4 text-3xl  ">
+                        {{ $reservation->day->format('n/j') }}
+                    </label>
+                    <label class="flex justify-center items-center text-center w-1/4 text-3xl mr-4">
+                        {{ $reservation->start_time->format('G:i') }} 〜
+                    </label>
+                    <a href="{{ route('users.mentors.messages.index', [$reservation->user, $reservation->mentor]) }}"
+                        class="flex justify-center invisible items-center text-center bg-green-400 hover:bg-green-500 text-white font-bold py-2 px-4 rounded">
+                        メッセージ
+                    </a>
+                </div>
+                <hr class="boild">
+                @endif
                 @endforeach
             </div>
         </div>
