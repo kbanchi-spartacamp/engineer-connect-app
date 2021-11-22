8175eb9c7f11e99f05608b38e03e8ff27abec0e0
kbanchi1111 <kbanchi.1111@gmail.com>
2021-11-22 10:37:42 +0900

    add reservation mail

--- app/Mail/SendMail.php
+++ app/Mail/SendMail.php
@@ -2,6 +2,7 @@
 
 namespace App\Mail;
 
+use App\Consts\MailConst;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
@@ -16,9 +17,10 @@ class SendMail extends Mailable
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($template, $variable)
     {
-        //
+        $this->template = $template;
+        $this->variable = $variable;
     }
 
     /**
@@ -28,8 +30,16 @@ class SendMail extends Mailable
      */
     public function build()
     {
-        return $this->view('emails.mail')
+        if ($this->template == MailConst::RESERVATION) {
+            return $this->view('emails.reservation')
+                ->from('noeconn@nowconn.co.jp', 'Test')
+                ->subject('New Reservation')
+                ->with(['variable' => $this->variable]);
+        }
+
+        return $this->view('emails.test')
             ->from('noeconn@nowconn.co.jp', 'Test')
-            ->subject('This is a test mail');
+            ->subject('This is a test mail')
+            ->with(['variable' => $this->variable]);
     }
 }
